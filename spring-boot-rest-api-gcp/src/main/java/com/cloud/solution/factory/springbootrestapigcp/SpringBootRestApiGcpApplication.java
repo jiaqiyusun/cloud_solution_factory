298e64da86a91749258a210166ec6f149e6f0f9c
jiaqiyusun <jiaqi.yu.sun@gmail.com>
2022-07-15 20:30:09 +0100

    initial project

---
+++ spring-boot-rest-api-gcp/src/main/java/com/cloud/solution/factory/springbootrestapigcp/SpringBootRestApiGcpApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.solution.factory.springbootrestapigcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiGcpApplication.class, args);
+	}
+
+}
---
+++ spring-boot-rest-api-gcp/src/test/java/com/cloud/solution/factory/springbootrestapigcp/SpringBootRestApiGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.solution.factory.springbootrestapigcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
